90834f010e0da06c63678b886affb204415f1125
Bill Fraser <wfraser@codewise.org>
2017-02-06 13:01:58 -0800

    make the proto example work a bit better, using the StreamProcess combinator

--- examples/proto.rs
+++ examples/proto.rs
@@ -17,6 +17,7 @@ use tokio_proto::streaming::{Message, Body};
 use tokio_service::Service;
 use tokio_uds::*;
 
+use std::collections::HashMap;
 use std::fs;
 use std::io;
 
@@ -29,11 +30,29 @@ fn umask(mask: u32) -> u32 {
 // Experimental code that will all go in the main crate once it's working:
 //
 
-struct FastcgiService {
+struct FastcgiService<F> {
     core_handle: Handle,
+    handler: F,
 }
 
-impl Service for FastcgiService {
+impl<F> FastcgiService<F>
+        where F: FnMut(HashMap<String, String>,
+                       Box<Stream<Item = FastcgiRecord, Error = io::Error>>)
+                -> Box<Future<Item = Message<FastcgiRecord, Body<FastcgiRecord, io::Error>>, Error = io::Error>>
+{
+    pub fn new(core_handle: Handle, handler: F) -> FastcgiService<F> {
+        FastcgiService {
+            core_handle: core_handle,
+            handler: handler,
+        }
+    }
+}
+
+impl<F> Service for FastcgiService<F>
+        where F: FnMut(HashMap<String, String>,
+                       Box<Stream<Item = FastcgiRecord, Error = io::Error>>)
+                -> Box<Future<Item = Message<FastcgiRecord, Body<FastcgiRecord, io::Error>>, Error = io::Error>>
+{
     type Request = Message<FastcgiRecord, Body<FastcgiRecord, io::Error>>;
     type Response = Message<FastcgiRecord, Body<FastcgiRecord, io::Error>>;
     type Error = io::Error;
@@ -41,7 +60,14 @@ impl Service for FastcgiService {
 
     fn call(&self, req: Self::Request) -> Self::Future {
 
-        let (id, input_record_stream) = match req {
+        // Conceptually:
+        //
+        // Read records from the body stream until the empty Params record,
+        //  building up the params hash map.
+        // Then pass the hash map and the remaining stream to the handler function,
+        //  which returns a response with body stream.
+
+        let (id, input_record_stream): (u16, Body<FastcgiRecord, io::Error>) = match req {
             Message::WithoutBody(_) => {
                 // All requests take up more than one record, so we're not using these.
                 panic!("unexpected WithoutBody response: {:?}", req);
@@ -54,66 +80,85 @@ impl Service for FastcgiService {
             }
         };
 
-        // Send something now, then read the rest of the records and do the rest of the response in
-        // the spawned function.
-        let data = "X-Powered-By: tokio-fastcgi/0.1\r\n";
-        let out = EasyBuf::from(Vec::from(data.as_bytes()));
+        let params_map = HashMap::<String, String>::new();
 
-        let (body_sender, body) = Body::pair();
-
-        let resp = Message::WithBody(
-            FastcgiRecord {
-                request_id: id,
-                body: FastcgiRecordBody::Stdout(out),
-            },
-            body);
-
-        self.core_handle.spawn_fn(move || {
-            input_record_stream.collect()
-                .then(move |records| {
-                    debug!("{:?}", records);
-
-                    let data = concat!(
-                        "Content-Type: text/plain\r\n",
-                        "\r\n",
-                        "Hello!"
-                    );
-                    let buf = EasyBuf::from(Vec::from(data.as_bytes()));
-
-                    body_sender.send(Ok(FastcgiRecord {
-                        request_id: id,
-                        body: FastcgiRecordBody::Stdout(buf)
-                    }))
-                })
-                .then(move |sender_result| {
-                    let end_records = vec![
-                        Ok(Ok(FastcgiRecord {
-                            request_id: id,
-                            body: FastcgiRecordBody::Stdout(EasyBuf::new()),
-                        })),
-                        Ok(Ok(FastcgiRecord {
-                            request_id: id,
-                            body: FastcgiRecordBody::Stderr(EasyBuf::new()),
-                        })),
-                        Ok(Ok(FastcgiRecord {
-                            request_id: id,
-                            body: FastcgiRecordBody::EndRequest(EndRequest {
-                                app_status: 0,
-                                protocol_status: ProtocolStatus::RequestComplete,
-                            })
-                        })),
-                    ];
-
-                    debug!("sending end records");
-                    sender_result.unwrap().send_all(stream::iter(end_records))
-                })
-                .then(|_| {
-                    debug!("done sending end records");
-                    future::ok(())
-                })
+        let stream_process = StreamProcess::new(input_record_stream, params_map, move |record, params_map| {
+            match record.body {
+                FastcgiRecordBody::Params(ref params) => {
+                    if params.is_empty() {
+                        debug!("done reading params");
+                        true
+                    } else {
+                        debug!("consuming a params record");
+                        for &(ref name_buf, ref value_buf) in params {
+                            let name = String::from_utf8_lossy(name_buf.as_slice()).into_owned();
+                            let value = String::from_utf8_lossy(value_buf.as_slice()).into_owned();
+                            params_map.insert(name, value);
+                        }
+                        false
+                    }
+                },
+                _ => panic!("unexpected record while reading params: {:?}", record)
+            }
         });
 
-        future::ok(resp).boxed()
+        let handle = self.core_handle.clone();
+
+        Box::new(stream_process.then(move |result| {
+            let (body_record_stream, params) = result.unwrap();
+            debug!("making the response: {:?}", params);
+
+            // Doesn't work because lifetimes...
+            //(self.handler)(params, body_record_stream.boxed())
+
+            let data = format!(
+                concat!(
+                    "X-Powered-By: tokio-fastcgi/0.1\r\n",
+                    "Content-Type: text/plain\r\n",
+                    "\r\n",
+                    "Hello from {:?}!"),
+                params["REQUEST_URI"]);
+            let out = EasyBuf::from(Vec::from(data.as_bytes()));
+
+            let (body_sender, body) = Body::pair();
+
+            let resp = Message::WithBody(
+                FastcgiRecord {
+                    request_id: id,
+                    body: FastcgiRecordBody::Stdout(out),
+                },
+                body);
+
+            handle.spawn_fn(move || {
+                let end_records = vec![
+                    Ok(Ok(FastcgiRecord {
+                        request_id: id,
+                        body: FastcgiRecordBody::Stdout(EasyBuf::new()),
+                    })),
+                    Ok(Ok(FastcgiRecord {
+                        request_id: id,
+                        body: FastcgiRecordBody::Stderr(EasyBuf::new()),
+                    })),
+                    Ok(Ok(FastcgiRecord {
+                        request_id: id,
+                        body: FastcgiRecordBody::EndRequest(EndRequest {
+                            app_status: 0,
+                            protocol_status: ProtocolStatus::RequestComplete,
+                        })
+                    })),
+                ];
+
+                debug!("sending end records");
+                body_sender.send_all(stream::iter(end_records))
+                    .then(|_| {
+                        debug!("done sending end records");
+                        future::ok(())
+                    })
+            });
+
+            debug!("sending response");
+            Ok(resp)
+        }))
     }
 }
 
@@ -138,9 +183,13 @@ fn main() {
     let srv = listener.incoming().for_each(move |(socket, _addr)| {
         println!("{:#?}", socket);
 
-        let service = FastcgiService {
-            core_handle: handle.clone(),
-        };
+        let service = FastcgiService::new(handle.clone(), |_params, _body_record_stream| {
+            // TODO: this currently isn't actually run because of lifetime problems noted above.
+            future::ok(Message::WithoutBody(FastcgiRecord {
+                request_id: 0,
+                body: FastcgiRecordBody::Stdout(EasyBuf::new())
+            })).boxed()
+        });
         let proto = FastcgiProto;
         proto.bind_server(&handle, socket, service);
 
